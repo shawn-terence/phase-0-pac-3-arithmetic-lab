784a03445e2657a48da4a7a34bb9679a7aa46fde
terence <terenceshawn18@outlook.com>
2023-09-28 08:23:23 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,53 @@
+
+function add(a, b) {
+    // we'll fill this in shortly
+ return a+b;
+
+  }
+  const answer1= add(1000,862);
+  console.log(answer1);
+
+  function subtract(c,d) {
+    // we'll fill this in shortly
+    return c-d
+  }
+  const answer2=(1000 , 1154)
+  console.log(answer2)
+
+  function multiply(e, f) {
+    // we'll fill this in shortly
+    return e * f
+  }
+  const answer3=multiply(566644, 1)
+  console.log(answer3)
+
+  function divide(g, h) {
+    // we'll fill this in shortly
+   return g / h
+  }
+  const  answer4= divide( 0.3381578947368421,1)
+  console.log(answer4)
+
+  function increment(n){
+    return n +1
+  }
+  const answer5=increment(788,)
+  console.log(answer5)
+
+  function decrement(n){
+    return n-1
+  }
+  const answer6=decrement(8)
+  console.log(answer6)
+
+  function makeInt(n){
+    return parseInt(n,10)
+  }
+  const answer7= makeInt("7")
+
+  function preserveDecimal(n){
+     return parseFloat(n)
+
+}
+const answer8=preserveDecimal(3.14)
+
